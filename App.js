c92467ae5c6ce9b4f4a0f7feb2d19864b5c0872d
athish1808 <93860935+athish1808@users.noreply.github.com>
2022-05-23 21:05:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React,{useReducer} from "react";
-import ComponentA from "./Components/Global/ComponentA";
-import ComponentB from "./Components/Global/ComponentB";
-import ComponentC from "./Components/Global/ComponentC";
-
+import ComponentA from "./Components/ComponentA";
+import ComponentB from "./Components/ComponentB";
+import ComponentC from "./Components/ComponentC";
+import Datafetching from "./Datafetching";
 export const CountContext = React.createContext()
 
 
@@ -24,13 +24,14 @@ function App() {
   const [ count, dispatch ] = useReducer(reducer,initialState)
   return (
     <CountContext.Provider 
-      value={{ countState:count , countDispatch:dispatch }}
-    >
+      value={{ countState:count , countDispatch:dispatch }}>
+    
           <div className="App">
             Count - { count }
             <ComponentA />
             <ComponentB />
             <ComponentC />
+            <Datafetching/>
           </div>
     </CountContext.Provider>
 
